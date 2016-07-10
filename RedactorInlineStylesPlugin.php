1e4ff567e743e01489d1e68ea69504bd40d32a85
carlcs <seelbach@gmx.de>
2016-07-10 09:44:44 +0200

    Prepare 2.1.0 release

--- RedactorInlineStylesPlugin.php
+++ RedactorInlineStylesPlugin.php
@@ -10,7 +10,7 @@ class RedactorInlineStylesPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '2.0.5';
+        return '2.1.0';
     }
 
     public function getSchemaVersion()
