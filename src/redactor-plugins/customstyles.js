495be8cf71d0821f04648bc51efddfa65d218fa6
carlcs <seelbach@gmx.de>
2022-05-25 13:10:28 +0200

    Cleanup

--- src/redactor-plugins/customstyles.js
+++ src/redactor-plugins/customstyles.js
@@ -1,76 +1,74 @@
-(function($R) {
+(function ($R) {
     $R.add('plugin', 'customstyles', {
-        init: function(app) {
+        init(app) {
             this.app = app;
-            this.lang = app.lang;
-            this.opts = app.opts;
-            this.toolbar = app.toolbar;
-
-            this.buttons = this.opts.customStyles || this.opts.customstyles || {};
-            this.defaultAddAfter = this.opts.customStylesDefaultAddAfter || this.opts.customstylesdefaultaddafter || 'italic';
-            this.customButtonIcons = this.opts.customButtonIcons || this.opts.custombuttonicons || {};
+            this.buttons = app.opts.customStyles || app.opts.customstyles || {};
+            this.defaultAddAfter =
+                app.opts.customStylesDefaultAddAfter ||
+                app.opts.customstylesdefaultaddafter ||
+                'italic';
+            this.customButtonIcons =
+                app.opts.customButtonIcons || app.opts.custombuttonicons || {};
             this.icons = Craft.RedactorCustomStyles.icons;
         },
-        start: function() {
-            // Add new buttons in reverse order, for expected sort order when
-            // addAfter isn’t used.
-            var keys = Object.keys(this.buttons).reverse();
 
-            for (var i = 0; i < keys.length; i++) {
-                var key = keys[i];
-                var config = this.buttons[key];
+        start() {
+            Object.entries(this.buttons)
+                .reverse()
+                .forEach(([key, config]) => {
+                    if (config.dropdown !== undefined) {
+                        this.addDropdown(key, config);
+                    } else {
+                        this.addButton(key, config);
+                    }
+                });
 
-                if (key.charAt(0) === '_') {
-                    continue;
+            Object.entries(this.customButtonIcons).forEach(([key, icon]) => {
+                const button = this.app.toolbar.getButton(key);
+                if (button) {
+                    button.setIcon(this.getIconHtml(icon));
                 }
-
-                if ('dropdown' in config) {
-                    this.addDropdown(key, config);
-                } else {
-                    this.addButton(key, config);
-                }
-            }
-
-            // Override icons
-            for (key in this.customButtonIcons) {
-                var $button = this.toolbar.getButton(key);
-
-                if ($button) {
-                    var icon = this.customButtonIcons[key];
-                    $button.setIcon(this.getIconHtml(icon));
-                }
-            }
+            });
         },
-        addButton: function(key, config) {
-            var data = {
+
+        addButton(key, config) {
+            const data = {
                 title: Craft.t('redactor-custom-styles', config.title),
                 icon: this.getIconHtml(config.icon || key),
                 api: config.api || 'module.inline.format',
                 args: config.args,
             };
 
-            this.toolbar.addButtonAfter(config.addAfter || this.defaultAddAfter, key, data);
+            this.app.toolbar.addButtonAfter(
+                config.addAfter || this.defaultAddAfter,
+                key,
+                data
+            );
         },
-        addDropdown: function(key, config) {
-            var data = {
+
+        addDropdown(key, config) {
+            const data = {
                 title: Craft.t('redactor-custom-styles', config.title),
                 icon: this.getIconHtml(config.icon || key),
                 dropdown: {},
             };
 
-            for (var itemKey in config.dropdown) {
-                var itemConfig = config.dropdown[itemKey];
-
+            Object.entries(config.dropdown).forEach(([itemKey, itemConfig]) => {
                 data.dropdown[itemKey] = {
                     title: Craft.t('redactor-custom-styles', itemConfig.title),
                     api: itemConfig.api || 'module.inline.format',
                     args: itemConfig.args,
                 };
-            }
+            });
 
-            this.toolbar.addButtonAfter(config.addAfter || this.defaultAddAfter, key, data);
+            this.app.toolbar.addButtonAfter(
+                config.addAfter || this.defaultAddAfter,
+                key,
+                data
+            );
         },
-        getIconHtml: function(icon) {
+
+        getIconHtml(icon) {
             return this.icons[icon] || null;
         },
     });
